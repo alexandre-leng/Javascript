688fd846669e882dc6ffc9216ff15aef3fe04de9
Alexandre Orlandini <72550137+orionp99@users.noreply.github.com>
2021-02-23 12:17:10 +0100

    65

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,16 +1,7 @@
 $(function(){
 
-    $("#p1").on({
-        mouseenter:function(){
-            $('#p5').hide();
-        },
-        mouseleave:function(){
-            $('#p5').show();
-        },
-        click: function(){
-            $('#p4').hide();
-        }
-    });
+    $("button").click(){
+        $("button").animate(arg1, arg2, arg3);
+    }
        
-
 });
